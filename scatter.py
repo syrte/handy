13011c071773633f2ec41e434d2790249491ba8f
syrte <styr.py@gmail.com>
2016-10-18 21:58:29 +0800

    add `cov_ellipses`, `lines`

--- scatter.py
+++ scatter.py
@@ -2,9 +2,10 @@ from __future__ import division, print_function, absolute_import
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Ellipse, Rectangle
-from matplotlib.collections import PatchCollection
+from matplotlib.collections import PatchCollection, LineCollection
 
-__all__ = ['circles', 'ellipses', 'rectangles']
+
+__all__ = ['circles', 'ellipses', 'rectangles', 'lines', 'cov_ellipses']
 
 
 def circles(x, y, s, c='b', vmin=None, vmax=None, **kwargs):
@@ -53,6 +54,7 @@ def circles(x, y, s, c='b', vmin=None, vmax=None, **kwargs):
     if np.isscalar(c):
         kwargs.setdefault('color', c)
         c = None
+
     if 'fc' in kwargs:
         kwargs.setdefault('facecolor', kwargs.pop('fc'))
     if 'ec' in kwargs:
@@ -128,6 +130,7 @@ def ellipses(x, y, w, h=None, rot=0.0, c='b', vmin=None, vmax=None, **kwargs):
     if np.isscalar(c):
         kwargs.setdefault('color', c)
         c = None
+
     if 'fc' in kwargs:
         kwargs.setdefault('facecolor', kwargs.pop('fc'))
     if 'ec' in kwargs:
@@ -207,6 +210,7 @@ def rectangles(x, y, w, h=None, rot=0.0, c='b', vmin=None, vmax=None, **kwargs):
     if np.isscalar(c):
         kwargs.setdefault('color', c)
         c = None
+
     if 'fc' in kwargs:
         kwargs.setdefault('facecolor', kwargs.pop('fc'))
     if 'ec' in kwargs:
@@ -240,3 +244,113 @@ def rectangles(x, y, w, h=None, rot=0.0, c='b', vmin=None, vmax=None, **kwargs):
     if c is not None:
         plt.sci(collection)
     return collection
+
+
+def lines(xy, c='b', vmin=None, vmax=None, **kwargs):
+    """
+    xy : sequence of array 
+        Coordinates of points in lines.
+        `xy` is a sequence of array (line0, line1, ..., lineN) where
+            line = [(x0, y0), (x1, y1), ... (xm, ym)]
+    c : color or sequence of color, optional, default : 'b'
+        `c` can be a single color format string, or a sequence of color
+        specifications of length `N`, or a sequence of `N` numbers to be
+        mapped to colors using the `cmap` and `norm` specified via kwargs.
+        Note that `c` should not be a single numeric RGB or RGBA sequence
+        because that is indistinguishable from an array of values
+        to be colormapped. (If you insist, use `color` instead.)
+        `c` can be a 2-D array in which the rows are RGB or RGBA, however.
+    vmin, vmax : scalar, optional, default: None
+        `vmin` and `vmax` are used in conjunction with `norm` to normalize
+        luminance data.  If either are `None`, the min and max of the
+        color array is used.
+    kwargs : `~matplotlib.collections.Collection` properties
+        Eg. alpha, linewidth(lw), linestyle(ls), norm, cmap, transform, etc.
+
+    Returns
+    -------
+    collection : `~matplotlib.collections.LineCollection`
+    """
+    if np.isscalar(c):
+        kwargs.setdefault('color', c)
+        c = None
+
+    if 'ls' in kwargs:
+        kwargs.setdefault('linestyle', kwargs.pop('ls'))
+    if 'lw' in kwargs:
+        kwargs.setdefault('linewidth', kwargs.pop('lw'))
+
+    collection = LineCollection(xy, **kwargs)
+    if c is not None:
+        collection.set_array(np.asarray(c))
+        collection.set_clim(vmin, vmax)
+
+    ax = plt.gca()
+    ax.add_collection(collection)
+    ax.autoscale_view()
+    plt.draw_if_interactive()
+    if c is not None:
+        plt.sci(collection)
+    return collection
+
+
+def cov_ellipses(x, y, cov_mat=None, cov_tri=None, q=None, nsig=None, **kwargs):
+    """Draw covariance error ellipses.
+    Parameters
+    ----------
+    x : array (n,)
+    y : array (n,)
+    cov_mat : array (n, 2, 2)
+    cov_tri : list of array (n,)
+        (xvar, yvar, xycov)
+    q : scalar or array
+    nsig : scalar or array
+    kwargs :
+        `ellipses` properties.
+        e.g. c, vmin, vmax
+    Reference
+    ---------
+    [1]: http://www.visiondummy.com/2014/04/draw-error-ellipse-representing-covariance-matrix
+    [2]: http://stackoverflow.com/questions/12301071/multidimensional-confidence-intervals
+    """
+    from scipy.stats import norm, chi2
+
+    if cov_mat is not None:
+        cov_mat = np.asarray(cov_mat)
+    elif cov_tri is not None:
+        assert len(cov_tri) == 3
+        cov_mat = np.transpose([[cov_tri[0], cov_tri[2]],
+                                [cov_tri[2], cov_tri[1]]])
+    else:
+        raise ValueError('One of `cov_mat` and `cov_tri` should be specified.')
+
+    x, y = np.asarray(x), np.asarray(y)
+    assert cov_mat.shape[:-2] == x.shape == y.shape
+    assert cov_mat.shape[-2:] == (2, 2)
+
+    if q is not None:
+        q = np.asarray(q)
+    elif nsig is not None:
+        q = 2 * norm.cdf(nsig) - 1
+    else:
+        raise ValueError('One of `q` and `nsig` should be specified.')
+    rho = chi2.ppf(q, 2)
+
+    val, vec = np.linalg.eigh(cov_mat)
+    rho = rho.reshape(rho.shape + (1,) * val.ndim)  # raise dimentions
+    axis = 2 * np.sqrt(val * rho)
+    w, h = axis[..., 0], axis[..., 1]
+    rot = np.degrees(np.arctan2(vec[..., 1, 0], vec[..., 0, 0]))
+
+    return ellipses(x, y, w, h, rot=rot, **kwargs)
+    """cov_cross
+    assert q.ndim == 0
+    res = []
+    xy = np.stack([x, y], -1)[..., None, :]
+    w_line = xy + vec[..., None, :, 0] * w[..., None, None] * np.array([[-0.5], [0.5]])
+    h_line = xy + vec[..., None, :, 1] * h[..., None, None] * np.array([[-0.5], [0.5]])
+    res.append(lines(w_line, **kwargs))
+    res.append(lines(h_line, **kwargs))
+
+    return res
+    """
