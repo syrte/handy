be477b5ae6665283744b663a05bc44dc83b1d824
syrte <styr.py@gmail.com>
2017-04-05 15:56:11 +0400

    add func

--- funcs.py
+++ funcs.py
@@ -1,10 +1,12 @@
 from __future__ import division, print_function, absolute_import
 from collections import Mapping, Iterable
 from functools import wraps
+import traceback
 import sys
 import gc
 
-__all__ = ['unpack_args', 'callback_gc', 'catch_exception', 'print_flush']
+__all__ = ['unpack_args', 'callback_gc', 'catch_exception', 'full_traceback',
+           'print_flush']
 
 
 def unpack_args(func):
@@ -38,6 +40,23 @@ def catch_exception(func):
         except Exception as msg:
             print("failed:  %s(*%s, **%s)\nmessage: %s" %
                   (func.__name__, args, kwargs, msg))
+            traceback.print_exc()
+    return wrapper
+
+
+def full_traceback(func):
+    """
+    Seems to not not necessary in Python 3
+    http://stackoverflow.com/a/29442282
+    http://bugs.python.org/issue13831
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            msg = "{}\n\nOriginal {}".format(e, traceback.format_exc())
+            raise type(e)(msg)
     return wrapper
 
 
@@ -47,3 +66,4 @@ def print_flush(*args, **kwargs):
     file = kwargs.get('file', sys.stdout)
     if flush and file is not None:
         file.flush()
+
