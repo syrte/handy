c10243987fe1537dc2504c7bad75f2e0849b8bec
syrte <styr.py@gmail.com>
2017-08-27 23:33:12 +0800

    new function `timeout`

--- funcs.py
+++ funcs.py
@@ -4,9 +4,11 @@ from functools import wraps
 import traceback
 import sys
 import gc
+import signal
+from concurrent.futures import ThreadPoolExecutor, TimeoutError
 
 __all__ = ['unpack_args', 'callback_gc', 'catch_exception', 'full_traceback',
-           'print_flush']
+           'print_flush', 'timeout']
 
 
 def unpack_args(func):
@@ -65,3 +67,68 @@ def print_flush(*args, **kwargs):
     file = kwargs.get('file', sys.stdout)
     if flush and file is not None:
         file.flush()
+
+
+class timeout:
+    """
+    Handling timeout.
+
+    Note signal.signal can only be called from the main thread.
+    If not the case, should use the decorator mode with `thread=True`.
+
+    Examples
+    --------
+        import time
+
+        # context mode
+        with timeout(seconds=3):
+            time.sleep(4)
+
+        # decorator mode
+        @timeout()
+        def func():
+            time.sleep(4)
+        func()
+
+    Reference
+    ---------
+    https://stackoverflow.com/a/22348885/2144720
+    https://stackoverflow.com/a/2282656/2144720
+    """
+
+    def __init__(self, seconds=1, exception=TimeoutError('Timeout.'),
+                 thread=False):
+        """
+        thread :
+            True: use concurrent.futures.ThreadPoolExecutor
+            False : use signal.signal
+        """
+        self.seconds = seconds
+        if isinstance(exception, Exception):
+            self.exception = exception
+        else:
+            self.exception = TimeoutError(exception)
+        self.thread = thread
+
+    def handler(self, signum, frame):
+        raise self.exception
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handler)
+        signal.alarm(self.seconds)
+
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if not self.thread:
+                with self:
+                    return func(*args, **kwargs)
+            else:
+                with ThreadPoolExecutor(1) as pool:
+                    res = pool.submit(func, *args, **kwargs)
+                    res.set_exception(self.exception)
+                    return res.result(self.seconds)
+        return wrapper
