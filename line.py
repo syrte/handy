ee9ce04b53b1989cec6766c066596328fc42327d
syrte <styr.py@gmail.com>
2018-03-14 23:41:52 +0800

    new function axline

--- line.py
+++ line.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from functools import wraps
 
-__all__ = ["abline", "ABLine2D"]
+__all__ = ["abline", "ABLine2D", "axline"]
 
 
 class ABLine2D(Line2D):
@@ -25,7 +25,7 @@ class ABLine2D(Line2D):
                 y = y0 + (y1 - y0) / (x1 - x0) * (x - x0)
         Additional arguments are passed to the <matplotlib.lines.Line2D> constructor.
 
-        It will have wrong behaiver when the axis-limits are
+        It will have wrong behavior when the axis-limits are
         changed by setting ticks. This can be corrected by call
         `xlim` to reset the limits.
         Refer this issue:
@@ -51,7 +51,7 @@ class ABLine2D(Line2D):
         else:
             ax = plt.gca()
         if not ('color' in kwargs or 'c' in kwargs):
-            kwargs.update(ax._get_lines.prop_cycler.next())
+            kwargs.update(next(ax._get_lines.prop_cycler))
 
         super(ABLine2D, self).__init__([], [], *args, **kwargs)
         self._point = tuple(point)
@@ -89,3 +89,84 @@ class ABLine2D(Line2D):
 @wraps(ABLine2D.__init__, assigned=['__doc__'], updated=[])
 def abline(a, b, *args, **kwargs):
     return ABLine2D(a, b, *args, **kwargs)
+
+
+def axline(a, b, **kwargs):
+    """
+    Add an infinite straight line across an axis.
+
+    Parameters
+    ----------
+    a, b: scalar or tuple
+        Acceptable forms are
+        y0, b:
+            y = y0 + b * x
+        (x0, y0), b:
+            y = y0 + b * (x - x0)
+        (x0, y0), (x1, y1):
+            y = y0 + (y1 - y0) / (x1 - x0) * (x - x0)
+    Additional arguments are passed to the <matplotlib.lines.Line2D> constructor.
+
+    Returns
+    -------
+    :class:`~matplotlib.lines.Line2D`
+
+    Other Parameters
+    ----------------
+    Valid kwargs are :class:`~matplotlib.lines.Line2D` properties,
+    with the exception of 'transform':
+    %(Line2D)s
+
+    Examples
+    --------
+    * Draw a thick red line with slope 1 and y-intercept 0::
+        >>> axline(0, 1, linewidth=4, color='r')
+    * Draw a default line with slope 1 and y-intercept 1::
+        >>> axline(1, 1)
+
+    See Also
+    --------
+    axhline : for horizontal lines
+    axvline : for vertical lines
+
+    Notes
+    -----
+    Currently this method does not work properly with log scaled axes.
+    Taken from https://github.com/matplotlib/matplotlib/pull/9321
+    """
+    from matplotlib import pyplot as plt
+    import matplotlib.transforms as mtransforms
+    import matplotlib.lines as mlines
+
+    if np.isscalar(a):
+        if not np.isscalar(b):
+            raise ValueError("Invalid line parameters.")
+        point, slope = (0, a), b
+    elif np.isscalar(b):
+        if not len(a) == 2:
+            raise ValueError("Invalid line parameters.")
+        point, slope = a, b
+    else:
+        if not len(a) == len(b) == 2:
+            raise ValueError("Invalid line parameters.")
+        if b[0] != a[0]:
+            point, slope = a, (b[1] - a[1]) / (b[0] - a[0])
+        else:
+            point, slope = a, np.inf
+
+    ax = plt.gca()
+    if "transform" in kwargs:
+        raise ValueError("'transform' is not allowed as a kwarg; "
+                         "axline generates its own transform.")
+    xtrans = mtransforms.BboxTransformTo(ax.viewLim)
+    viewLimT = mtransforms.TransformedBbox(
+        ax.viewLim,
+        mtransforms.Affine2D().rotate_deg(90).scale(-1, 1))
+    ytrans = (mtransforms.BboxTransformTo(viewLimT) +
+              mtransforms.Affine2D().scale(slope).translate(*point))
+    trans = mtransforms.blended_transform_factory(xtrans, ytrans)
+    line = mlines.Line2D([0, 1], [0, 1],
+                         transform=trans + ax.transData,
+                         **kwargs)
+    ax.add_line(line)
+    return line
