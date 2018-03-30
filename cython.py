8ec4a14631d85c76a5930c483404be176481dfe2
syrte <styr.py@gmail.com>
2018-03-31 02:38:35 +0800

    go on fix

--- cython.py
+++ cython.py
@@ -338,7 +338,7 @@ def cythonmagic(code, export=None, name=None, force=False,
     if name is None:
         key = (code, cythonize_args, args, environ, os.environ,
                sys.executable, sys.version_info, Cython.__version__)
-        str = unicode if hasattr(__builtins__, 'unicode') else str
+        str = unicode if hasattr(__builtins__, 'unicode') else __builtins__.str
         hashed = hashlib.md5(str(key).encode('utf-8')).hexdigest()
         ext_name = "_cython_magic_{}".format(hashed)
     else:
