cfeeadbdf7fd96d865e47d00a245463fada35fa0
syrte <styr.py@gmail.com>
2018-11-06 21:53:59 +0800

    update dpeak

--- cluster.py
+++ cluster.py
@@ -19,10 +19,14 @@ class DensPeakFinder:
     import numpy as np
     from matplotlib import pyplot as plt
 
-    pts = np.random.randn(1000, 3)
-    pts[:300, :2] += [3, 1]
+    n, d = 1000, 3
+    pts = np.random.randn(n, d)
+    pts[:int(n*0.3), :2] *= 0.5
+    pts[:int(n*0.3), :2] += [3, 1]
+    pts[int(n*0.3):int(n*0.5), :2] += [1, 3]
+
     dpeak = DensPeakFinder(pts, k=10)
-    peak, ix_peak, group = dpeak.plot_peak(400)
+    peak, ix_peak, group = dpeak.plot_peak()
     # if you don't want the plot
     peak, ix_peak, group = dpeak.find_peak(400, cluster=True)
     """
@@ -46,8 +50,10 @@ class DensPeakFinder:
 
         Todos
         -----
-        Check references of optimal choice of k and gamma
+        Optimal choice of k and gamma
         Performance optimization with Cython or Numba
+        Substructure within density saddle point
+        Labeling the noise
         """
         if (k is not None) and (r is not None):
             raise ValueError("Only one of 'k' or 'r' can be specified!")
@@ -72,31 +78,33 @@ class DensPeakFinder:
         # delta
         delta = np.full(npts, Rmax, dtype='float')
         chief = np.full(npts, -1, dtype='int')  # superior neighbor
-        if kmax is not None:
-            dists, index = tree.query(pts, kmax, return_distance=True)
+        if kmax is not None or rmax is not None:
+            if kmax is not None:
+                dists, index = tree.query(
+                    pts, kmax, return_distance=True, sort_results=True)
+            else:
+                index, dists = tree.query_radius(
+                    pts, rmax, return_distance=True, sort_results=True)
             for i in range(npts):
+                rho_i = rho[i]
                 for j, dist in zip(index[i], dists[i]):
-                    if (rho[j] > rho[i]):
-                        chief[i] = j
-                        delta[i] = dist
+                    if (rho[j] > rho_i):
+                        chief_i, delta_i = j, dist
                         break
-        elif rmax is not None:
-            index, dists = tree.query_radius(pts, rmax, return_distance=True)
-            for i in range(npts):
-                for j, dist in zip(index[i], dists[i]):
-                    if (rho[j] > rho[i]) and (dist < delta[i]):
-                        chief[i] = j
-                        delta[i] = dist
+                chief[i], delta[i] = chief_i, delta_i
         else:
             dists = squareform(pdist(pts))
             for i in range(npts):
+                rho_i, delta_i = rho[i], delta[i]
                 for j, dist in enumerate(dists[i]):
-                    if (rho[j] > rho[i]) and (dist < delta[i]):
-                        chief[i] = j
-                        delta[i] = dist
+                    if (rho[j] > rho_i) and (dist < delta_i):
+                        chief_i, delta_i = j, dist
+                chief[i], delta[i] = chief_i, delta_i
 
         # gamma
-        gamma = rho * delta**ndim  # need sphere_coeff?
+        gamma = sphere_coeff * rho * delta**ndim  # need sphere_coeff?
+        sorted_index = np.argsort(gamma)
+        sorted_gamma = gamma[sorted_index]
 
         # properties
         self.npts = npts
@@ -106,15 +114,20 @@ class DensPeakFinder:
         self.delta = delta
         self.gamma = gamma
         self.chief = chief
+        self.sorted_index = sorted_index
+        self.sorted_gamma = sorted_gamma
 
-    def get_gamma_threshold(self, method=None):
+    def get_gamma_threshold(self, gamma_th=None):
         # XXX
-        gamma = self.gamma
-        lg_gamma = np.log10(gamma)
-        gamma_threshold = 10**(np.nanmean(lg_gamma) + 4.5 * np.nanstd(lg_gamma))
-        return gamma_threshold
+        if gamma_th is None:
+            gamma = self.gamma
+            lg_gamma = np.log10(gamma)
+            gamma_threshold = 10**(np.nanmean(lg_gamma) + 4.5 * np.nanstd(lg_gamma))
+            return gamma_threshold
+        else:
+            return gamma_th
 
-    def find_peak(self, gamma_th=None, rho_th=None, cluster=False):
+    def find_peak(self, gamma_th=None, npeak=None, rho_th=None, cluster=False):
         """
         Parameters
         ----------
@@ -134,13 +147,21 @@ class DensPeakFinder:
         group : array
             Group id of each point, start from 0.
         """
-        if gamma_th is None:
-            gamma_th = self.get_gamma_threshold()
-
         if rho_th is None:
-            ix_peak = np.where((self.gamma >= gamma_th))[0]
+            sorted_index = self.sorted_index
+            sorted_gamma = self.sorted_gamma
         else:
-            ix_peak = np.where((self.gamma >= gamma_th) & (self.rho >= rho_th))[0]
+            ix_rho_th = np.where(self.rho[self.sorted_index] >= rho_th)[0]
+            sorted_index = self.sorted_index[ix_rho_th]
+            sorted_gamma = self.sorted_gamma[ix_rho_th]
+
+        if npeak is not None:
+            ix_peak = sorted_index[-npeak:][::-1]
+        else:
+            if gamma_th is None:
+                gamma_th = self.get_gamma_threshold()
+            ix_th = np.searchsorted(sorted_gamma, gamma_th, side='right')
+            ix_peak = sorted_index[ix_th:][::-1]
         peak = self.pts[ix_peak]
         npeak = len(ix_peak)
 
@@ -163,7 +184,7 @@ class DensPeakFinder:
         else:
             return peak, ix_peak
 
-    def plot_peak(self, gamma_th=None, rho_th=None, axes=[0, 1]):
+    def plot_peak(self, gamma_th=None, npeak=None, rho_th=None, axes=[0, 1]):
         """
         Show the decision graph and return peaks.
 
@@ -176,16 +197,17 @@ class DensPeakFinder:
         axes : list of length 2
             Specify the axes of n-d data points to show.
         """
-        if gamma_th is None:
+        if npeak is None and gamma_th is None:
             gamma_th = self.get_gamma_threshold()
         if len(axes) != 2:
             raise ValueError("Argument 'axes' should be shape (2,)")
 
         dpeak = self
-        peak, ix_peak, group = dpeak.find_peak(gamma_th, rho_th=rho_th, cluster=True)
+        peak, ix_peak, group = dpeak.find_peak(
+            gamma_th, npeak=npeak, rho_th=rho_th, cluster=True)
         npeak = len(peak)
 
-        xlims, ylims = np.quantile(dpeak.pts, q=[0.05, 0.95], axis=0).T[axes]
+        xlims, ylims = np.percentile(dpeak.pts, q=[5, 95], axis=0).T[axes]
 
         plt.figure(figsize=(12, 8))
         plt.subplots_adjust(wspace=0.2, hspace=0.2)
@@ -200,7 +222,7 @@ class DensPeakFinder:
         plt.ylabel(r'$X%d$' % axes[1])
 
         plt.subplot(224)
-        plt.scatter(*dpeak.pts.T[axes], c=dpeak.rho, s=5, vmax=np.quantile(dpeak.rho, q=0.9))
+        plt.scatter(*dpeak.pts.T[axes], c=dpeak.rho, s=5, vmax=np.percentile(dpeak.rho, q=90))
         plt.colorbar(label=r'$\rho$')
         plt.scatter(*peak.T[axes], s=150, lw=3, c='k', marker='x')
         plt.xlim(xlims)
@@ -213,24 +235,29 @@ class DensPeakFinder:
         plt.yscale('log')
         plt.scatter(dpeak.rho, dpeak.delta, s=5)
         plt.scatter(dpeak.rho[ix_peak], dpeak.delta[ix_peak], s=150, lw=3, c='k', marker='x')
-        xlims = np.array(plt.gca().get_xlim())
-        plt.plot(xlims, (gamma_th / xlims)**(1 / dpeak.ndim), ls='--', color='gray')
+        if gamma_th is not None:
+            xlims = np.array(plt.gca().get_xlim())
+            plt.plot(xlims, (gamma_th / xlims)**(1 / dpeak.ndim), ls='--', color='gray')
         if rho_th is not None:
             plt.axvline(rho_th, ls='--', color='gray')
         plt.xlabel(r'$\rho$')
         plt.ylabel(r'$\delta$')
 
         plt.subplot(223)
-        gamma_sorted = np.sort(dpeak.gamma)
+        gamma_sorted = dpeak.sorted_gamma
         gamma_sorted_mid = np.sqrt(gamma_sorted[1:] * gamma_sorted[:-1])
-        dlngamma_dlnN = -np.diff(np.log(np.arange(dpeak.npts, 0, -1))) / np.diff(np.log(gamma_sorted))
+        n_cum = np.arange(dpeak.npts, 0, -1)
+        dlngamma_dlnN = -np.diff(np.log(n_cum)) / np.diff(np.log(gamma_sorted))
+
         plt.xscale('log')
         plt.yscale('log')
-        plt.scatter(gamma_sorted, np.arange(dpeak.npts, 0, -1), s=5)
-        plt.axvline(gamma_th, ls='--', color='gray')
+        plt.scatter(gamma_sorted, n_cum, s=5)
+        if gamma_th is not None:
+            plt.axvline(gamma_th, ls='--', color='gray')
         plt.xlabel(r'$\gamma=\delta^d\rho$')
         plt.ylabel(r'$N(>\gamma)$')
+
         plt.twinx()
         plt.plot(gamma_sorted_mid[-10:], dlngamma_dlnN[-10:], ls='--', lw=0.75, color='gray')
-        plt.ylim(0, 1)
+        plt.ylim(0, 2)
         return peak, ix_peak, group
