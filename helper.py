07aa1c912bd8fc7f3b8b5a99510393c3da22c542
syrte <styr.py@gmail.com>
2019-05-16 00:08:22 +0800

    add function mulegend

--- helper.py
+++ helper.py
@@ -1,14 +1,26 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import axes, docstring
 
-__all__ = ["axtext", 'errorbar2']
+__all__ = ['axtext', 'mulegend', 'errorbar2']
 
 
+@docstring.copy_dedent(axes.Axes.text)
 def axtext(x, y, s, *args, **kwargs):
     ax = plt.gca()
     kwargs.setdefault('transform', ax.transAxes)
     return ax.text(x, y, s, *args, **kwargs)
+
+
+@docstring.copy_dedent(axes.Axes.legend)
+def mulegend(*args, **kwargs):
+    """Multiple legend
+    """
+    ax = plt.gca()
+    ret = plt.legend(*args, **kwargs)
+    ax.add_artist(ret)
+    return ret
 
 
 def errorbar2(x, y, yerr=None, xerr=None, **kwds):
